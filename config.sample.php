1e40c6edf7b629dc17ac140597643c7286092323
spelhate <sebastien.pelhate@megalis.bretagne.bzh>
2022-04-01 10:50:24 +0200

    Ajout fichier de configuration exemple

---
+++ config.sample.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace connexion\connexion
+
+{
+    class Database
+    {
+        const DSN = "mysql:host=localhost;dbname=openrgpd";
+        const USERNAME = "YOURUSERNAME";
+        const PASSWORD = "YOURPASSWORD";
+    }
+}
+
+namespace
+
+{
+    class Application
+    {
+        const MAIL = "no-reply@YOURDOMAIN.COM";
+    }
+}
+?>

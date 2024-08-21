84f079d832a8eb0f1d24e654b0ecba93855aaa4b
syji35 <45880189+syji35@users.noreply.github.com>
2024-08-21 15:11:43 +0000

    modification du readme pour le mode docker compose

--- connexion/check_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_SESSION['identifiant'])){
-    header('Location: index.php');
-    exit();
-} else if (($_SESSION['admin']) == "contributeur") {
-    header('Location: visu.php');
-    exit();
-}
-?> 
--- connexion/check_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_SESSION['identifiant'])){
-    header('Location: index.php');
-    exit();
-}
-?>
